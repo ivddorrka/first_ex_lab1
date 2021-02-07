7927641ad7124ef1def8553de0bd081f809c5039
ivddorrka <ivddorrka@gmail.com>
2021-02-08 01:43:45 +0200

    Fixed all functions and added correct documentation

--- first.1.py
+++ first.1.py
@@ -1,5 +1,5 @@
 """
-
+Working on scyscrapers' problem
 """
 
 
@@ -8,8 +8,8 @@ def read_input(path: str):
     Read game board file from path.
     Return list of str.
 
-    >>> read_input("check.txt")
-    ['***21**', '452453*', '423145*', '*543215', '*35214*', '*41532*', '*2*1***']
+    # >>> read_input("check.txt")
+    # ['***21**', '452453*', '423145*', '*543215', '*35214*', '*41532*', '*2*1***']
     """
     file_txt = open(path, "r", encoding = "UTF-8")
     file_here = ''.join(list(file_txt)).split('\n')
@@ -34,23 +34,23 @@ def left_to_right_check(input_line: str, pivot: int):
     lst = list(input_line)
     if lst[-1] == "*":
         del lst[-1]
-    if pivot == int(lst[0]):
-        del lst[0]
-        int_lst = []
-        for i in lst:
-            int_lst.append(int(i))
-        res = []
-        res.append(int_lst[0])
-        for j in range(len(int_lst)-1):
-            if int_lst[j+1] > int_lst[j]:
-                res.append(int_lst[j+1])
-        if len(res) == pivot:
-            return True
+    del lst[0]
+    int_lst = []
+    for i in lst:
+        int_lst.append(int(i))
+    res = []
+    res.append(int_lst[0])
+    for j in range(len(int_lst)-1):
+        if int_lst[j+1] >= int_lst[j]:
+            res.append(int_lst[j+1])
+    if len(set(res)) == pivot:
+        return True
     else:
         return False
 # print(left_to_right_check("412453*", 4))
 # print(left_to_right_check("452453*", 5))
-
+# print(left_to_right_check("5124531", 5))
+# print(left_to_right_check("132345*", 3))
 
 def check_not_finished_board(board: list):
     """
@@ -58,11 +58,14 @@ def check_not_finished_board(board: list):
 
     Return True if finished, False otherwise.
 
-    >>> check_not_finished_board(['***21**', '4?????*', '4?????*', '*?????5', '*?????*', '*?????*', '*2*1***'])
+    >>> check_not_finished_board(['***21**', '4?????*', '4?????*', '*?????5',\
+    '*?????*', '*?????*', '*2*1***'])
     False
-    >>> check_not_finished_board(['***21**', '412453*', '423145*', '*543215', '*35214*', '*41532*', '*2*1***'])
+    >>> check_not_finished_board(['***21**', '412453*', '423145*', '*543215', \
+    '*35214*', '*41532*', '*2*1***'])
     True
-    >>> check_not_finished_board(['***21**', '412453*', '423145*', '*5?3215', '*35214*', '*41532*', '*2*1***'])
+    >>> check_not_finished_board(['***21**', '412453*', '423145*', '*5?3215',\
+    '*35214*', '*41532*', '*2*1***'])
     False
     """
     all_signs = ''.join(board)
@@ -70,9 +73,7 @@ def check_not_finished_board(board: list):
         return False
     else:
         return True
-# print(check_not_finished_board(['***21**', '4?????*', '4?????*', '*?????5', '*?????*', '*?????*', '*2*1***']))
-# print(check_not_finished_board(['***21**', '412453*', '423145*', '*5?3215', '*35214*', '*41532*', '*2*1***']))
-# print(check_not_finished_board(['***21**', '412453*', '423145*', '*543215', '*35214*', '*41532*', '*2*1***']))
+
 
 def check_uniqueness_in_rows(board: list):
     """
@@ -80,42 +81,48 @@ def check_uniqueness_in_rows(board: list):
 
     Return True if buildings in a row have unique length, False otherwise.
 
-    >>> check_uniqueness_in_rows(['***21**', '412453*', '423145*', '*543215', '*35214*', '*41532*', '*2*1***'])
+    >>> check_uniqueness_in_rows(['***21**', '412453*', '423145*', '*543215',\
+    '*35214*', '*41532*', '*2*1***'])
     True
-    >>> check_uniqueness_in_rows(['***21**', '452453*', '423145*', '*543215', '*35214*', '*41532*', '*2*1***'])
+    >>> check_uniqueness_in_rows(['***21**', '452453*', '423145*', '*543215',\
+    '*35214*', '*41532*', '*2*1***'])
     False
-    >>> check_uniqueness_in_rows(['***21**', '412453*', '423145*', '*553215', '*35214*', '*41532*', '*2*1***'])
+    >>> check_uniqueness_in_rows(['***21**', '412453*', '423145*', '*553215',\
+    '*35214*', '*41532*', '*2*1***'])
     False
     """
     del board[0]
     del board[-1]
+    lst = []
+    for i in board:
+        lst.append(list(i))
     res = []
-    for _ in board:
-        if _[0] == '*':
-            del list(_)[0]
-            if len(set(_)) == len(_):
-                res.append(_)
-        if _[-1] == '*':
-            del list(_)[-1]
-            if len(set(_)) == len(_):
-                res.append(_)
-    return res
-# print(check_uniqueness_in_rows(['000', '*124*', '123*']))
-
+    for _ in lst:
+        del _[-1]
+        del _[0]
+        for j in _:
+            if _.count(j) >= 2:
+                res.append(j)
+    if len(res) > 0:
+        return False
+    else:
+        return True
 
 def check_1(lst):
     """
+    checks whether the amount of visible skyscrapers is the equal to pivot
     >>> check_1(['4', '1', '2', '3', '5', '4', '*'])
     True
     """
+    if lst[-1] == '*':
+        del lst[-1]
     pivot = int(lst[0])
-    del lst[-1]
+    # del lst[-1]
     board = []
     for j in lst:
         board.append(int(j))
     res = [0]
     res.append(board[1])
-    
     for i in range(1, len(board)-1):
         if board[i] < board[i+1]:
             if board[i+1] > max(res):
@@ -127,7 +134,6 @@ def check_1(lst):
 
     else:
         return False
-# print(check_1([4, 2, 3, 1, 4, 5]))
 # print(check_1(['4', '1', '2', '3', '4', '0', '*']))
 
 def check_horizontal_visibility(board: list):
@@ -138,42 +144,48 @@ def check_horizontal_visibility(board: list):
      i.e., for line 412453* , hint is 4, and 1245 are the four buildings
       that could be observed from the hint looking to the right.
 
-    >>> check_horizontal_visibility(['***21**', '412453*', '423145*', '*543215', '*35214*', '*41532*', '*2*1***'])
+    >>> check_horizontal_visibility(['***21**', '412453*', '423145*', '*543215',\
+    '*35214*', '*41532*', '*2*1***'])
     True
-    >>> check_horizontal_visibility(['***21**', '452453*', '423145*', '*543215', '*35214*', '*41532*', '*2*1***'])
+    >>> check_horizontal_visibility(['***21**', '452453*', '423145*', '*543215',\
+    '*35214*', '*41532*', '*2*1***'])
     False
-    >>> check_horizontal_visibility(['***21**', '452413*', '423145*', '*543215', '*35214*', '*41532*', '*2*1***'])
+    >>> check_horizontal_visibility(['***21**', '452413*', '423145*', '*543215',\
+    '*35214*', '*41532*', '*2*1***'])
     False
     """
+    # del board[0]
+    # del board[-1]
     result = []
     res = []
     for i in board:
         res.append(list(i))
-    
     prom = []
-    for j in res:
-        if j[0] != '*' or j[-1] != '*':
-            prom.append(j)
+    for j in range(len(res)):
+        if res[j].count('*') < 2:
+            prom.append(res[j])
 
-    for _ in prom:
-        if _[0] == '*':
-            rev = _[::-1]
+    for _ in range(len(prom)):
+        if prom[_][0] == '*':
+            rev = prom[_][::-1]
             if check_1(rev) is True:
-                result.append(_)
-        else:
-            if check_1(_) is True:
-                result.append(_)
-    
+                result.append(prom[_])
+        if prom[_][-1] == '*':
+            if check_1(prom[_]) is True:
+                result.append(prom[_])
+        if prom[_].count('*') == 0:
+            if check_1(prom[_]) is True and check_1(prom[_][::-1]) is True:
+                result.append(prom[_])
     if len(result) == len(prom):
         return True
     else:
         return False
-# print(check_horizontal_visibility(['***21**', '452413*', '423145*', '*543215', '*35214*', '*41532*', '*2*1***']))
-# print(check_horizontal_visibility(['***21**', '412453*', '423145*', '*543215', '*35214*', '*41532*', '*2*1***']))
-
-
 def rebuilding(lst):
-    res = []
+    """
+    This function rebuilds columns into new row
+    >>> rebuilding(['**12*', '12345', '23456', '**23*'])
+    [['*', '1', '2', '*'], ['*', '2', '3', '*'], ['1', '3', '4', '2'], ['2', '4', '5', '3'], ['*', '5', '6', '*']]
+    """
     prom_res = []
     for i in lst:
         prom_res.append(list(i))
@@ -189,11 +201,10 @@ def rebuilding(lst):
     return result
 
 
-# print(rebuilding(['***21**', '412453*', '423145*', '*543215', '*35214*', '*41532*', '*2*1***']))
-
 def check_columns(board: list):
     """
-    Check column-wise compliance of the board for uniqueness (buildings of unique height) and visibility (top-bottom and vice versa).
+    Check column-wise compliance of the board for uniqueness, \
+    (buildings of unique height) and visibility (top-bottom and vice versa).
 
     Same as for horizontal cases, but aggregated in one function for vertical case, i.e. columns.
 
@@ -204,12 +215,12 @@ def check_columns(board: list):
     >>> check_columns(['***21**', '412553*', '423145*', '*543215', '*35214*', '*41532*', '*2*1***'])
     False
     """
-    if check_horizontal_visibility(rebuilding(board)) is True:
+    var1 = check_horizontal_visibility(rebuilding(board))
+    var2 = check_uniqueness_in_rows(rebuilding(board))
+    if var1 is True and var2 is True:
         return True
     else:
         return False
-# print(check_columns(['***21**', '412553*', '423145*', '*543215', '*35214*', '*41532*', '*2*1***']))
-# print(check_columns(['***21**', '412453*', '423145*', '*543215', '*35214*', '*41532*', '*2*1***']))
 
 
 def check_skyscrapers(input_path: str):
@@ -218,21 +229,24 @@ def check_skyscrapers(input_path: str):
     Return True if the board status is compliant with the rules,
     False otherwise.
 
-    >>> check_skyscrapers("check.txt")
-    True
     """
     lst = read_input(input_path)
-    check_1 = check_columns(lst)
+    checkk_1 = check_columns(lst)
     check_2 = check_horizontal_visibility(lst)
     check_3 = check_not_finished_board(lst)
     check_4 = check_uniqueness_in_rows(lst)
-    # if check_1 is True and check_2 is True and check_3 is True and check_4 is True:
-    #     return True
-    # else:
-    #     return False
+    if checkk_1 is True and check_2 is True and check_3 is True and check_4 is True:
+        return True
+    else:
+        return False
     # return check_4
 
-print(check_skyscrapers('skyscrapers_state.txt'))
+# print(check_skyscrapers('skyscrapers_state.txt'))
 
 # if __name__ == "__main__":
 #     print(check_skyscrapers("check.txt"))
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
